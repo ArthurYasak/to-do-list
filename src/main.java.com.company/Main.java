a3e6fb42e541d071a2bedb1ec657ddb4d44af747
Arthur <yasak97@mail.ru>
2022-11-21 11:17:57 +0300

    Init Application (OOP pack)

--- src/main.java.com.company/Main.java
+++ src/main.java.com.company/Main.java
@@ -1,201 +1,79 @@
 package main.java.com.company;
 
-import java.io.*;
-import java.nio.file.*;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.EOFException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.io.Serializable;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        // maybe create second file
-        System.out.println("Hello!");
-        System.out.println("Type your business:");
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
         Scanner sc = new Scanner(System.in);
 
-        Path path = Paths.get("path.txt");
-        try {
-            Files.createFile(path);
-        } catch (IOException e){}
-        String str = sc.nextLine() + "\n";
-        byte[] byteStr = str.getBytes();
-        Files.write(path, byteStr, StandardOpenOption.APPEND);
+        while (true) {    // user commands input (realize later)
 
-        System.out.println("Your to-do list:");
-
-        List<String> linesToOutput = Files.readAllLines(path);
-        for (String line : linesToOutput) {
-            System.out.println(line);
+            break;
         }
 
-        System.out.println("Type your business to delete (or press 0):");
-        int delBusinessNum = sc.nextInt();
+        ListsControl.addList();
 
-        // (LAST VERSION!) main part of program
-        /*
-        File output = new File("output.txt");
-        File copyOutput = new File("copyOutput.txt");
-        File newOutput = new File("newOutput.txt");
-        boolean create = newOutput.createNewFile();
-        if (create) {
-            System.out.println("File newOutput created");
-        } else {
-            System.out.println("File newOutput NOT created");
+        // *maybe* add this realization to ToDoList.addBusiness()
+        System.out.println("Available lists:");
+        for (ToDoList list : ListsControl.toDoLists) {
+            System.out.println(list.getName());
         }
-        boolean delete = newOutput.delete();
-        if (delete) {
-            System.out.println("File newOutput deleted");
-        } else {
-            System.out.println("File newOutput NOT deleted");
-        }
-
-         */
-
-        // тестовый файл   (CREATED, RENAME, DELETED)
-        /*
-        System.out.println();
-        File fileTest = new File("fileTest.txt");
-        boolean created = fileTest.createNewFile();
-        if(created) {
-            System.out.println("File has been created");
-        } else {
-            System.out.println("File has NOT been created");
-        }
-        FileWriter testWriter;
-        PrintWriter testPrintWriter;
-        // testWriter = new FileWriter(fileTest);
-        // testPrintWriter = new PrintWriter(testWriter);
-        System.out.println(str);
-        // testPrintWriter.println("str");
-        // переименуем тестовый файл
-
-        File newFileTest = new File("newFileTest.txt");
-        boolean renamed = fileTest.renameTo(newFileTest);
-        if (renamed) {
-            System.out.println("File has been renamed");
-        } else {
-            System.out.println("File has NOT been renamed");
-        }
-        // удалим тестовый файл
-        boolean deleted = newFileTest.delete();
-        if(deleted)
-            System.out.println("File has been deleted");
-        else
-            System.out.println("File has NOT been deleted");
-        System.out.println();
-
-         */
-
-
-        // work with Path
-        /*
-        System.out.println("Work with PATH");
-        Path path = Paths.get("path.txt");
-        Path newPath = Paths.get("newPath.txt");
-        // path.resolveSibling(newPath);
-        try {
-            Files.createFile(path);
-            // Files.createFile(newPath);
-            Files.copy(path, newPath);
-            System.out.println("Files PATH has been created");
-        }
-        catch (IOException e) {
-            System.out.println("Operation PATH FAILED");
-            // Files.delete(path);
-            // Files.delete(newPath);
-        }
-        byte[] byteStr = str.getBytes();
-        Files.write(path, byteStr, StandardOpenOption.APPEND);
-        System.out.println();
-
-
-
-        Path oldFile = Paths.get("oldFile.txt");
-        Path newFile = Paths.get("newFile.txt");
-        try {
-            Files.createFile(oldFile);
-            Files.move(oldFile, Paths.get("newFile.txt"));
-            System.out.println("oldFile successfully renamed to newFile");
-        }
-        catch (IOException e){
-            System.out.println("rename operation FAILED");
-            Files.delete(oldFile);
-            Files.delete(newFile);
-        }
-        // Files.move(copyOutput, copyOutput.resolveSibling("output.txt"));
-        System.out.println();
-
-         */
-
-
-
-
-        // WRITER ((LAST VERSION!) main part of program)
-        /*
-        FileWriter writer = new FileWriter(output, true);
-        FileWriter copyWriter = new FileWriter(copyOutput);
-        FileWriter newWriter = new FileWriter(newOutput);
-        writer.write(str + "\n");
-        writer.flush();
-        writer.close();
-
-         */
-
-
-        // deleteBusiness(1);
-        deleteBusiness(delBusinessNum);
-
-        deleteEmpty();
-
-        // do a method "printBusiness"
-        System.out.println("Your updated to-do list:");
-        linesToOutput = Files.readAllLines(path);
-        for (String line : linesToOutput) {
-            System.out.println(line);
-        }
-
-        // удалим третий элемент из списка дел
-        // Path newPath = Paths.get("newPath.txt");
-
-
-
-        // OUTPUT to-do-list
-        /*
-        FileReader reader = new FileReader(copyOutput);
-        BufferedReader buffReader = new BufferedReader(reader);
-        while (buffReader.ready()) {
-            System.out.println(buffReader.readLine());
-        }
-
-         */
-
-        /*
-        writer = new PrintWriter(new FileWriter("output.txt"));
-        int current = 0;
-        String line;
-        while ((line = buffReader.readLine()) != null) {
-            if (current != index) {
-                writer.println(line);
+        System.out.println("Write ToDoList name to add business");
+        String listToSearch = sc.nextLine();
+        ToDoList currentList = ListsControl.searchList(listToSearch);
+        if (currentList != null) {
+            Path path = Paths.get(currentList.getName() + ".txt");
+            System.out.println("How many businesses want you add");
+            int n = sc.nextInt();
+            sc.nextLine();
+            for (int i = 0; i < n; i++) {
+                currentList.addBusiness(path);
             }
-            current++;
+
+        } else {
+            System.out.println("List with this name does not exist");
         }
-        */
-        /*
-        int c;
-        while (reader.read() != -1) {
-            System.out.print(reader.read());
-        }
-        */
+
+        ListsControl.printLists();
+
 
     }
 
+    // for deleteBusiness
+    /*
+        System.out.println("Type your business to delete (or press 0):");
+        int delBusinessNum = 0;
+        if (sc.hasNextInt()) {
+            delBusinessNum = sc.nextInt();
+        } else {
+            System.out.println("Введено не число");
+        }
+
+         */
+
+    // methods for class ToDoList
+    /*
     public static void deleteBusiness(int index) throws IOException {
         Path path = Paths.get("path.txt");
         Path newPath = Paths.get("newPath.txt");
-        try {
+        if (!(Files.exists(newPath))) {
             Files.createFile(newPath);
-        } catch (IOException e) {}
+        }
         List<String> linesToRewrite = Files.readAllLines(path);
         for (int i = 0; i < linesToRewrite.size(); i++) {
             if (i != (index - 1)) {
@@ -219,46 +97,6 @@ public class Main {
         if (Files.exists(newPath)) {
             Files.delete(newPath);
         }
-
-        /*
-
-        Files.delete(path);
-
-        BufferedReader reader = null;
-        PrintWriter writer = null;
-
-        File output = new File("output.txt");
-        File copyOutput = new File("copyOutput.txt");
-        FileReader fReader = new FileReader(output);
-        reader = new BufferedReader(fReader);
-        FileWriter fWriter = new FileWriter(copyOutput);
-        writer = new PrintWriter(fWriter);
-
-        int current = 0;
-        String line;
-        while ((line = reader.readLine()) != null) {
-            if (current != index) {
-                writer.print(line + "\n");
-            }
-            current++;
-        }
-        */
-
-        // try to rename (false)
-        /*
-        File newOutput = new File("newOutput.txt");
-        FileWriter newWriter = new FileWriter(newOutput);
-        FileReader newReader = new FileReader(newOutput);
-        newWriter.close();
-        newReader.close();
-        boolean rename = copyOutput.renameTo(newOutput);
-        System.out.println(rename);
-
-
-        reader.close();
-        writer.close();
-
-         */
     }
 
     public static void deleteEmpty() throws IOException {
@@ -283,6 +121,259 @@ public class Main {
         if (Files.exists(newPath)) {
             Files.delete(newPath);
         }
+    }
+
+     */
+}
+
+class ListsControl {
+    private int amount; // for amount of added lists (now is not use)
+    private String listName;
+    static ArrayList<ToDoList> toDoLists = new ArrayList<>();
+    static void addList() throws IOException, ClassNotFoundException {
+        String toDoListsObj = "toDoListsObj.ser";
+        Path forSave = Paths.get(toDoListsObj);
+
+        // SERIALIZATION
+        FileOutputStream outputStream;
+        ObjectOutputStream objectOutputStream;
+        if (Files.exists(forSave)) {
+            // Stream to write data to file (connection stream)
+            outputStream = new FileOutputStream(toDoListsObj, true);
+
+            // Stream to convert ToDoList object to bytes (chain (цепной) stream) WITHOUT WRITING HEADER
+            objectOutputStream = new AppendingObjectOutputStream(outputStream);
+        } else {
+            // Stream to write data to file (connection stream)
+            outputStream = new FileOutputStream(toDoListsObj, true);
+
+            // Stream to convert ToDoList object to bytes (chain (цепной) stream)
+            objectOutputStream = new ObjectOutputStream(outputStream);
+        }
+
+        // DESERIALIZATION
+        // Stream to connect to file and to read data from this file (connection stream)
+        FileInputStream inputStream = new FileInputStream(toDoListsObj);
+
+        // Stream to convert bytes to ToDoList object (chain (цепной) stream)
+        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+        // filling the ArrayList from file
+        boolean flagForCheck = true;
+        while (inputStream.available() != 0) {
+            ToDoList listForCheck = new ToDoList();
+            try {
+                listForCheck = (ToDoList) objectInputStream.readObject();
+            } catch (EOFException e) {
+                System.out.println("End of file reached"); // print when reset() available
+            }
+            Path listCheck = Paths.get(listForCheck.getName() + ".txt");
+            if (Files.exists(listCheck)) {
+                toDoLists.add(listForCheck);
+            } else {
+                flagForCheck = false;
+            }
+        }
+        // checking if .txt files with ListsControl is not exist anymore
+        if (!flagForCheck) {
+            FileOutputStream rewriteStream = new FileOutputStream(toDoListsObj, false);
+            ObjectOutputStream rewriteObjectStream = new ObjectOutputStream(rewriteStream);
+            for (ToDoList thisToDoList: toDoLists) {
+                rewriteObjectStream.writeObject(thisToDoList);
+            }
+        }
+
+        Scanner sc = new Scanner(System.in);
+        ToDoList toDoList = new ToDoList();
+
+        System.out.println("Enter the name of list.");
+        String listName = sc.nextLine();
+        if (listName.isEmpty()) {
+            System.out.println("List name field is empty");
+            return;
+        }
+        toDoList.setName(listName);
+        Path path = Paths.get(toDoList.getName() + ".txt");
+        if (!(Files.exists(path))) {
+            System.out.println("Enter the date of list.");
+            toDoList.setDate(sc.nextLine());
+            System.out.println("Enter the description of list.");
+            toDoList.setDescription(sc.nextLine());
+            Files.createFile(path);
+            objectOutputStream.writeObject(toDoList); // Save ToDoList object to file
+            toDoLists.add(toDoList);
+            // outputStream.close();
+            // objectOutputStream.close();
+            // System.out.println(inputStream.available());
+            System.out.println("List was successfully added.");
+
+            System.out.println("Do you want to add business in ToDoList" + toDoList.getName());
+            System.out.println("Press 1 (Yes) or 0 (No)");
+            if (sc.nextLine().equals("1")) {
+                System.out.println("How many businesses you want to add?");
+                int n = sc.nextInt();
+                sc.nextLine();
+                for (int i = 0; i < n; i++) {
+                    toDoList.addBusiness(path);
+                }
+            }
+
+        } else {
+            System.out.println("List already exists.");
+        }
+
+    }
+    static void printLists() {
+        System.out.print("Available " + toDoLists.size());
+        System.out.println((toDoLists.size() == 1)? " list:" : " lists:");
+        for (ToDoList toDoList: toDoLists) {
+            System.out.println(toDoList.getName());
+            System.out.println("Date: " + toDoList.getDate());
+            System.out.println("Description: " + toDoList.getDescription());
+            System.out.println();
+        }
+    }
+
+    // old version for one list, need to rewrite
+    void printList() throws IOException {
+        Path path = Paths.get("path.txt");
+        List<String> linesToOutput = Files.readAllLines(path);
+        for (String line : linesToOutput) {
+            System.out.println(line);
+        }
+    }
+
+    public static ToDoList searchList(String listName) {
+        for (ToDoList toDoListForCheck : toDoLists) {
+            if (toDoListForCheck.getName().equals(listName)) {
+                return toDoListForCheck;
+            }
+        }
+        return null;
+    }
+
+    public void setListName(String listName) {
+        this.listName = listName;
+    }
+    public String getListName() {
+        return this.listName;
+    }
+}
+
+class ToDoList implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private String date;
+    private String description;
+    static ArrayList<Business> busList = new ArrayList<>();
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public String getDate() {
+        return this.date;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+
+    void addBusiness(Path path) throws IOException {
+
+        // String businessObj = "businessObj.ser"; // want to try to use Data Base
+
+        System.out.println("Print your business");
+        Scanner sc = new Scanner(System.in);
+        Business business = new Business();
+        business.setName(sc.nextLine());
+        System.out.println("Set importance for " + business.getName() + "\n" +
+                "HIGH    -  press 1 \n" +
+                "MEDIUM  -  press 2 \n" +
+                "LOW     -  press 3 \n" +
+                "or just press \"Enter\" to set default importance: MEDIUM");
+
+        if (business.setImportance(sc.nextLine())) {
+            System.out.println("For business " + business.getName() + " set " + business.getImportance() + " importance");
+        } else {
+            System.out.println("For business " + business.getName() + " stayed " + business.getImportance() + " importance");
+        }
+
+        String str = business.getName() + "\n";
+        byte[] byteStr = str.getBytes();
+        Files.write(path, byteStr, StandardOpenOption.APPEND);
+        System.out.println("Business added!");
+    }
+
+    void printList() throws IOException {
+        Path path = Paths.get("path.txt");
+        List<String> linesToOutput = Files.readAllLines(path);
+        for (String line : linesToOutput) {
+            System.out.println(line);
+        }
+    }
+}
+
+
+
+class Business {
+    private String name;
+    private Importance importance = Importance.MEDIUM;
+
+    /**
+     * Set importance for the Deal
+     *
+     * @param importance
+     * @return result of Set importance (true or false)
+     */
+    public boolean setImportance(String importance) {
+        switch (importance) {
+            case "1":
+                this.importance = Importance.HIGH;
+                return true;
+            case "2":
+                this.importance = Importance.MEDIUM;
+                return true;
+            case "3":
+                this.importance = Importance.LOW;
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public Importance getImportance() {
+        return importance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+enum Importance {
+    HIGH,
+    MEDIUM,
+    LOW
+}
+
+class AppendingObjectOutputStream extends ObjectOutputStream {
+    public AppendingObjectOutputStream(OutputStream out) throws IOException {
+        super(out);
+    }
+    @Override
+    protected void writeStreamHeader() throws IOException {
+        // reset(); // when active EOFException throws
     }
 }
 
